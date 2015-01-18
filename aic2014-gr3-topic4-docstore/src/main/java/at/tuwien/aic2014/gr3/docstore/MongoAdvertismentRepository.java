8ae2ba82994323e5395e6e5f069a0048508bac9f
Seijan <cputzhtl@gmail.com>
2015-01-18 18:47:04 +0100

    adding advertisment if user has interests, simple approach

--- aic2014-gr3-topic4-docstore/src/main/java/at/tuwien/aic2014/gr3/docstore/MongoAdvertismentRepository.java
+++ aic2014-gr3-topic4-docstore/src/main/java/at/tuwien/aic2014/gr3/docstore/MongoAdvertismentRepository.java
@@ -1,30 +1,37 @@
 package at.tuwien.aic2014.gr3.docstore;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
-import com.mongodb.DB;
-
 import at.tuwien.aic2014.gr3.domain.Advertisment;
 import at.tuwien.aic2014.gr3.domain.UserTopic;
 import at.tuwien.aic2014.gr3.shared.AdvertismentRepository;
+import com.mongodb.DB;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 @Repository
 public class MongoAdvertismentRepository implements AdvertismentRepository {
 
-	@Autowired
-	private DB db;
-	
-	@Override
-	public Collection<Advertisment> findByInterests(List<UserTopic> interests,
-			int max) {
-		//TODO christoph
-		//throw new UnsupportedOperationException("not yet implemented");
-		return new ArrayList<Advertisment>();
-	}
+    @Autowired
+    private DB db;
+
+    @Override
+    public Collection<Advertisment> findByInterests(List<UserTopic> interests,
+                                                    int max) throws UnsupportedOperationException {
+        ArrayList<Advertisment> list = new ArrayList<Advertisment>();
+        Advertisment car = new Advertisment("", Arrays.asList(new String[]{"car", "auto", "bmw", "audi", "vehicle"}));
+
+        while (list.size() < max) {
+            if (interests.contains("car")) {
+                list.add(car);
+            }
+        }
+        //TODO christoph
+        //throw new UnsupportedOperationException("not yet implemented");
+        return list;
+    }
 
 }
--- aic2014-gr3-topic4-shared/src/main/java/at/tuwien/aic2014/gr3/domain/Advertisment.java
+++ aic2014-gr3-topic4-shared/src/main/java/at/tuwien/aic2014/gr3/domain/Advertisment.java
@@ -1,8 +1,51 @@
 package at.tuwien.aic2014.gr3.domain;
 
+import java.util.List;
+
 /**
  * an ad
  */
 public class Advertisment {
 
+    String url;
+    List<String> tags;
+
+    public Advertisment(String url, List<String> tags){
+        this.url = url;
+        this.tags = tags;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public void setSingleTag(String tag){
+        this.tags.add(tag);
+    }
+
+    public String getSingleTag(int index){
+        return this.tags.get(index);
+    }
+
+    public String toString(){
+        String tagString = tags.get(0) + ",";
+        for (int i = 1;tags.size()<(i-1);i++){
+            tagString += tags.get(i) + ",";
+        }
+        tagString += tags.get(tags.size()-1);
+        return "Advertisment [URL=" + this.url + ", tags="
+                + tagString + "]";
+    }
 }
