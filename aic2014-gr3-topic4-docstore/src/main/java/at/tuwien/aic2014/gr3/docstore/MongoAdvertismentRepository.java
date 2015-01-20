9a3ae3ef0a102339ae66bbf4b8e0b90589a4096b
Seijan <cputzhtl@gmail.com>
2015-01-20 21:09:47 +0100

    Test for Single User with 3 Interests FindByInterest Methode implementiert, kann noch verbessert werden

--- aic2014-gr3-topic4-docstore/src/main/java/at/tuwien/aic2014/gr3/docstore/MongoAdvertismentRepository.java
+++ aic2014-gr3-topic4-docstore/src/main/java/at/tuwien/aic2014/gr3/docstore/MongoAdvertismentRepository.java
@@ -1,5 +1,15 @@
 package at.tuwien.aic2014.gr3.docstore;
 
+
+import at.tuwien.aic2014.gr3.domain.Advertisment;
+import at.tuwien.aic2014.gr3.domain.IHasTopic;
+import at.tuwien.aic2014.gr3.shared.AdvertismentRepository;
+import com.mongodb.*;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Repository;
+
 import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -9,51 +19,91 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import org.apache.commons.io.IOUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.stereotype.Repository;
-
-import at.tuwien.aic2014.gr3.domain.Advertisment;
-import at.tuwien.aic2014.gr3.domain.IHasTopic;
-import at.tuwien.aic2014.gr3.shared.AdvertismentRepository;
-
-import com.mongodb.BasicDBList;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-
 @Repository
 public class MongoAdvertismentRepository implements AdvertismentRepository {
 
     @Autowired
-    private static DB db;
+    private DB db;
 
     @Override
     public Collection<Advertisment> findByInterests(List<? extends IHasTopic> interests,
                                                     int max) throws UnsupportedOperationException {
         ArrayList<Advertisment> list = new ArrayList<Advertisment>();
-
-        while (list.size() < max) {
-
+        if (db.collectionExists("ads")) {
+            DBCollection collection = db.getCollection("ads");
+            DBCursor basic = collection.find();
+            basic.next();
+            while (basic.hasNext()) {
+                DBObject obj = basic.curr();
+                BasicDBList dblist = (BasicDBList) obj.get("tags");
+                for (Object dblistitem : dblist) {
+                    for (IHasTopic interest : interests) {
+                        if (interest.getTopic().equals((String) dblistitem)) {
+                            byte[] pic = (byte[]) obj.get("image");
+                            String[] tags = new String[dblist.size()];
+                            int i = 0;
+                            for (Object tag : dblist) {
+                                tags[i] = (String) tag;
+                                i++;
+                            }
+                            if (list.size() < max) {
+                                list.add(new Advertisment(pic, tags));
+                            }
+                            System.out.println("ADD " + dblistitem);
+                        }
+                    }
+                }
+                basic.next();
+            }
         }
-        //TODO christoph
-        //throw new UnsupportedOperationException("not yet implemented");
         return list;
     }
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(DocStoreConfig.class);
         MongoAdvertismentRepository ad = ctx.getBean(MongoAdvertismentRepository.class);
-        BasicDBList list = new BasicDBList();
+        ad.save();
+    }
+
+    private static String[] readFile(String path) {
+        String[] tags = null;
         try {
-            Files.walk(Paths.get("D:\\Banner")).forEach(filePath -> {
+            FileInputStream is = new FileInputStream(path + ".txt");
+
+            List<String> lines = IOUtils.readLines(is);
+            for (String line : lines) {
+                tags = line.split(",");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return tags;
+    }
+
+    public void save() {
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try {
+            Files.walk(Paths.get("C:\\Banner")).forEach(filePath -> {
+                BasicDBList list = new BasicDBList();
                 if (Files.isRegularFile(filePath)) {
                     String path = filePath.toString().toLowerCase();
-                    if(path.endsWith(".jpg")){
-                        String[] tags = readFile(path);
-                        for(String tag : tags){
-                            list.add(tag);
-                            System.out.println(tag);
+                    if (path.endsWith(".jpg")) {
+                        String[] tags = readFile(filePath.toString());
+                        if (tags != null) {
+                            for (String tag : tags) {
+                                list.add(tag);
+                            }
+                        }
+                        try {
+                            FileInputStream is = new FileInputStream(filePath.toString());
+                            IOUtils.copy(is, out);
+                            byte[] data = out.toByteArray();
+                            BasicDBObject image_jo = new BasicDBObject().append("image", data);
+                            image_jo.append("tags", list);
+                            db.getCollection("ads").save(image_jo);
+                        } catch (IOException e) {
+                            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
                         }
                     }
                 }
@@ -61,30 +111,5 @@ public class MongoAdvertismentRepository implements AdvertismentRepository {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        BasicDBObject jo = new BasicDBObject().append("tags", list);
-        db.getCollection("ads").save(jo);
-    }
-
-    private static String[] readFile(String path) {
-        String[] tags = null;
-        try {
-            FileInputStream is = new FileInputStream(path + ".txt");
-            List<String> lines = IOUtils.readLines(is);
-            for(String line : lines){
-                tags =  line.split(",");
-            }
-            ByteArrayOutputStream output = new ByteArrayOutputStream();
-            
-			IOUtils.copy(is, output);
-			output.toByteArray();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return tags;
-    }
-
-    private void save() {
-
     }
 }
---
+++ aic2014-gr3-topic4-docstore/src/test/java/at/tuwien/aic2014/gr3/docstore/MongoAdvertismentRepositoryTest.java
@@ -0,0 +1,64 @@
+package at.tuwien.aic2014.gr3.docstore;
+
+import at.tuwien.aic2014.gr3.domain.Advertisment;
+import at.tuwien.aic2014.gr3.domain.UserTopic;
+import com.mongodb.DB;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: putzchri
+ * Date: 19.01.15
+ * Time: 19:08
+ * To change this template use File | Settings | File Templates.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = DocStoreConfig.class)
+public class MongoAdvertismentRepositoryTest  {
+
+    @Autowired
+    private DB db;
+
+    @Before
+    public void clearCollection() {
+        if ("aicdocstore-test".equals(db.getName())) {
+            db.getCollection("ads").drop();
+        }
+    }
+
+    @Test
+    public void testInterestsOfSingleUser(){
+        Map<String,Object> map = new HashMap();
+        map.put("to","car");
+        map.put("cnt",10);
+        UserTopic interest = new UserTopic(map);
+        map.put("to","bmw");
+        map.put("cnt",5);
+        UserTopic interest1 = new UserTopic(map);
+        map.put("to","girls");
+        map.put("cnt",100);
+        UserTopic interest2 = new UserTopic(map);
+
+        List<UserTopic> interests = new ArrayList<>();
+        interests.add(interest);
+        interests.add(interest1);
+        interests.add(interest2);
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(DocStoreConfig.class);
+        MongoAdvertismentRepository mongoAdvertisment = ctx.getBean(MongoAdvertismentRepository.class);
+        mongoAdvertisment.save();
+        Collection<Advertisment> list = mongoAdvertisment.findByInterests(interests, 5);
+        if (list.isEmpty()){
+            Assert.fail("List should not be null!");
+        }
+    }
+}
